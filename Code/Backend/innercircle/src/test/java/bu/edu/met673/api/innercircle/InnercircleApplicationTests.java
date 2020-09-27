6fcfb4b8430f97b0c158366ce76ec67a9584a259
wasupol_pp <wasut@bu.edu>
2020-09-27 14:53:57 -0400

    add project

---
+++ Code/Backend/innercircle/src/test/java/bu/edu/met673/api/innercircle/InnercircleApplicationTests.java
@@ -0,0 +1,13 @@
+package bu.edu.met673.api.innercircle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnercircleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
