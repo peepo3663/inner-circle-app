5597e4b334e6587fdb70acf032eb0a2398c76a16
Michael Ranasinghe <michael.ranasinghe1@gmail.com>
2020-11-10 13:49:58 -0500

    Update GetAllFirebaseUsers so that all emails be displayed at initial search

--- Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/notifications/NotificationsFragment.kt
+++ Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/notifications/NotificationsFragment.kt
@@ -2,14 +2,19 @@ package edu.bu.metcs673.project.ui.notifications
 
 import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.FirebaseDatabase
@@ -24,6 +29,7 @@ import edu.bu.metcs673.project.R
 import edu.bu.metcs673.project.adapter.chat.SearchAdapter
 import edu.bu.metcs673.project.model.chat.MessageModel
 import edu.bu.metcs673.project.model.chat.UserEmailModel
+import kotlinx.android.synthetic.main.fragment_notifications.*
 
 
 class NotificationsFragment : Fragment() {
@@ -32,6 +38,8 @@ class NotificationsFragment : Fragment() {
     private val mUsers = mutableListOf<UserEmailModel>()
     private val usersRef: CollectionReference = Firebase.firestore.collection("users")
     private var snapshotListener: ListenerRegistration? = null
+    private var recyclerView:RecyclerView?=null
+    private var searchEmailText: EditText?=null
 
 
     /*
@@ -59,8 +67,30 @@ class NotificationsFragment : Fragment() {
 
         val root = inflater.inflate(R.layout.fragment_notifications, container, false)
 
+        recyclerView= view?.findViewById(R.id.searchuseremaiList)
+        recyclerView!!.setHasFixedSize(true)
+        recyclerView!!.layoutManager=LinearLayoutManager(context)
+        searchEmailText=view?.findViewById(R.id.searchuseremail)
+
         mUsers.clear()
         GetAllFirebaseUsers()
+
+        searchEmailText!!.addTextChangedListener(object: TextWatcher
+        {
+            override fun afterTextChanged(s: Editable?) {
+                TODO("Not yet implemented")
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                searchforUserEmail(s.toString().toLowerCase())
+            }
+
+        })
+
         return root
 
         //return super.onCreateView(inflater, container, savedInstanceState)
@@ -74,15 +104,20 @@ class NotificationsFragment : Fragment() {
 
 
         snapshotListener = usersRef.addSnapshotListener { querySnapshot, _ ->
-            if (querySnapshot != null && !querySnapshot.isEmpty) {
-                //Iterate through all registered users in application
-                for (eachuser in querySnapshot.documents) {
-                    val eachuserObject = eachuser.toObject(UserEmailModel::class.java)
-                    if (eachuserObject != null) {
-                        mUsers.add(eachuserObject)
+            if (querySnapshot != null && !querySnapshot.isEmpty)
+            {
+                if (searchEmailText!!.text.toString()=="")
+                {
+                    //Iterate through all registered users in application
+                    for (eachuser in querySnapshot.documents) {
+                        val eachuserObject = eachuser.toObject(UserEmailModel::class.java)
+                        if (eachuserObject != null) {
+                            mUsers.add(eachuserObject)
+                        }
                     }
+                    searchadapter=SearchAdapter(context!!,mUsers!!)
+                    recyclerView!!.adapter =searchadapter
                 }
-                searchadapter=SearchAdapter(context!!,mUsers!!)
             }
         }
     }
@@ -103,6 +138,7 @@ class NotificationsFragment : Fragment() {
                     }
                 }
                 searchadapter=SearchAdapter(context!!,mUsers!!)
+                recyclerView!!.adapter =searchadapter
             }
 
         }
