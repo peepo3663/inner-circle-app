8019dadfabd383406a29455b4f12ba51cd219322
wasupol_pp <wasut@bu.edu>
2020-11-13 18:05:41 -0500

    clean code

--- Code/Android/app/src/main/java/edu/bu/metcs673/project/adapter/chat/SearchAdapter.kt
+++ Code/Android/app/src/main/java/edu/bu/metcs673/project/adapter/chat/SearchAdapter.kt
@@ -11,38 +11,25 @@ import edu.bu.metcs673.project.model.chat.UserEmailModel
 
 //@brief Class implementation of adaptor used for notification fragment
 //  By default, we must override three functions: getItemCount, onCreateViewHolder, onBindHolder
-class SearchAdapter (mContext: Context, mUsers:List<UserEmailModel>): RecyclerView.Adapter<CustomViewHolder?>(){
-
-    private val mContext:Context
-    private val mUsers:List<UserEmailModel>
-
-    init {
-        this.mContext=mContext
-        this.mUsers=mUsers
-    }
+class SearchAdapter(private val mContext: Context, private val mUsers: List<UserEmailModel>) :
+    RecyclerView.Adapter<CustomViewHolder?>() {
 
     override fun getItemCount(): Int {
         return mUsers.size
     }
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): CustomViewHolder {
-        val view: View= LayoutInflater.from(mContext).inflate(R.layout.user_search_display,viewGroup,false)
-        //val cellforrow= LayoutInflater.from(mContext).inflate(R.layout.user_search_display,viewGroup,false)
+        val view: View =
+            LayoutInflater.from(mContext).inflate(R.layout.user_search_display, viewGroup, false)
         return CustomViewHolder(view)
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-       val userprofile = mUsers[position]
-       holder.useremail.text=userprofile.email
+        val userprofile = mUsers[position]
+        holder.userEmail.text = userprofile.email
     }
 }
 
-class CustomViewHolder(val view: View): RecyclerView.ViewHolder(view){
-
-    lateinit var useremail: TextView
-    init {
-        useremail=itemView.findViewById(R.id.useremail)
-    }
-
-
+class CustomViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+    val userEmail: TextView = itemView.findViewById(R.id.useremail)
 }
