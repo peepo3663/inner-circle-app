f87fb9d87ad6c83510a2fd2c5ebc850442dd5a3d
Michael Ranasinghe <66277905+ranasm@users.noreply.github.com>
2020-11-30 09:59:14 -0500

    Initial Chat Test Checkpoint
    Up to this point, code has been developed to send messages to specific user. However, I still need to fix subtle bugs

---
+++ Code/Android/app/src/main/java/edu/bu/metcs673/project/adapter/chat/ChatsAdapter.kt
@@ -0,0 +1,74 @@
+package edu.bu.metcs673.project.adapter.chat
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.view.menu.ActionMenuItemView
+import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import edu.bu.metcs673.project.R
+import kotlinx.android.synthetic.main.message_item_left.view.*
+
+class ChatsAdapter(mContext: Context, mChatList: List<IndividualChat>) : RecyclerView.Adapter<ViewHolder?>()
+{
+    private val mContext: Context
+    private val mChatList: List<IndividualChat>
+    var firebaseUser: FirebaseUser= FirebaseAuth.getInstance().currentUser!!
+
+    init
+    {
+        this.mChatList=mChatList
+        this.mContext=mContext
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ViewHolder
+    {
+        return if (position ==1)
+        {
+            val view: View = LayoutInflater.from(mContext).inflate(R.layout.message_item_left, parent, false)
+            ViewHolder(view)
+        }
+        else
+        {
+            val view: View = LayoutInflater.from(mContext).inflate(R.layout.message_item_left, parent, false)
+            ViewHolder(view)
+        }
+    }
+
+    override fun getItemCount(): Int
+    {
+        return mChatList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int)
+    {
+        val chat: IndividualChat = mChatList[position]
+        holder.showtextmessage!!.text=chat.getMessage()
+
+    }
+
+    override fun getItemViewType(position: Int): Int {
+
+        return if (mChatList[position].getSender().equals(firebaseUser!!.uid))
+        {
+            1
+        }
+        else
+        {
+            0
+        }
+    }
+
+}
+
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+{
+    val showtextmessage :TextView= itemView.findViewById(R.id.show_text_message)
+    var textmessagename :TextView= itemView.findViewById(R.id.text_message_name)
+    var profileimage:ImageView= itemView.findViewById(R.id.image_message_profile)
+
+}
--- Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/chat/QueriedUserEmailChatFragment.kt
+++ Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/chat/QueriedUserEmailChatFragment.kt
@@ -19,7 +19,10 @@ import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import edu.bu.metcs673.project.R
+import edu.bu.metcs673.project.adapter.chat.ChatsAdapter
+import edu.bu.metcs673.project.adapter.chat.IndividualChat
 import edu.bu.metcs673.project.adapter.chat.SearchAdapter
+import edu.bu.metcs673.project.model.chat.MessageModel
 import edu.bu.metcs673.project.model.chat.UserEmailModel
 import kotlinx.android.synthetic.main.fragment_chat.*
 import kotlinx.android.synthetic.main.fragment_chat.user_text_chatbox
@@ -27,27 +30,44 @@ import kotlinx.android.synthetic.main.fragment_chat_query.*
 
 class QueriedUserEmailChatFragment : AppCompatActivity(){
 
+    //Initialize variables
     var sendButton: Button?=null
     var userIdVisit: String=""
     var username: String=""
     var firebaseUser: FirebaseUser?=null
     private val chatsRef: CollectionReference = Firebase.firestore.collection("chats")
     private lateinit var currentChatId: String
+    var chatsAdapter: ChatsAdapter?=null
+    var mChatList: List<IndividualChat>?=null
+    lateinit var recycler_view_chat: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.fragment_chat_query)
 
+        //Find view by ids
+        sendButton = findViewById(R.id.button_chatbox_send)
+        recycler_view_chat = findViewById(R.id.recyclerViewMessageList)
+
+        //Retrieve intent information
         intent=intent
         userIdVisit= intent.getStringExtra("visit_id").toString()
         username=intent.getStringExtra("username").toString()
+
         val firebaseUserId = FirebaseAuth.getInstance().currentUser?.uid
-
-        sendButton = findViewById(R.id.button_chatbox_send)
-
+        var linearLayoutManager = LinearLayoutManager(applicationContext)
 
         ReceiverName.text=username
+
+        //Set up recycler view
+        recycler_view_chat.setHasFixedSize(true)
+        linearLayoutManager.stackFromEnd=true
+        recycler_view_chat.layoutManager = linearLayoutManager
+
+        //Place retrieve function into appropriate setting
+        retrieveMessages(firebaseUserId,userIdVisit)
+
         sendButton?.setOnClickListener{
 
             val message=user_text_chatbox.text.toString()
@@ -64,6 +84,31 @@ class QueriedUserEmailChatFragment : AppCompatActivity(){
 
         }
 
+    }
+
+    private fun retrieveMessages(senderId: String?, receiverId: String)
+    {
+        mChatList=ArrayList()
+        val reference= chatsRef.document("12MCY06Cac6jg5dkOkgJ").collection("messages")
+
+        reference.addSnapshotListener{ messages, _ ->
+            (mChatList as ArrayList<IndividualChat>).clear()
+            if (messages != null && !messages.isEmpty)
+            {
+                for (message in messages.documents)
+                {
+                    val messageObject = message.toObject(IndividualChat::class.java)
+                    if (messageObject != null) {
+                        if (messageObject.getSender().equals(senderId) && messageObject.getReceiver().equals(receiverId)) {
+                            (mChatList as ArrayList<IndividualChat>).add(messageObject)
+                        }
+                    }
+                }
+                chatsAdapter= ChatsAdapter(this, mChatList as ArrayList<IndividualChat>)
+                recycler_view_chat.adapter=chatsAdapter
+            }
+
+        }
     }
 
     private fun sendMessagetoUser(senderId: String?, receiverId: String, message: String) {
