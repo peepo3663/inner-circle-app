19b21eb5e689675753dcb8b0cb070dd3cc805df7
PeterLakeSB52 <lakep83@gmail.com>
2020-12-03 02:54:31 -0500

    Initial setup for MainActivity UI test with Espresso

---
+++ Code/Android/app/src/androidTest/java/edu/bu/metcs673/project/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package edu.bu.metcs673.project
+
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java, false, false)
+
+    @Test
+    fun appLaunchesSuccessfully() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+}
