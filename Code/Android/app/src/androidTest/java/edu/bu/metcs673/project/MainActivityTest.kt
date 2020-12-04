1d69cd7562f2a471279c0ac4fdf74f48037b5afa
PeterLakeSB52 <lakep83@gmail.com>
2020-12-03 19:04:59 -0500

    MainActivity Nav_view test

--- Code/Android/app/src/androidTest/java/edu/bu/metcs673/project/MainActivityTest.kt
+++ Code/Android/app/src/androidTest/java/edu/bu/metcs673/project/MainActivityTest.kt
@@ -1,8 +1,11 @@
 package edu.bu.metcs673.project
 
 import androidx.test.core.app.ActivityScenario
+import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -23,4 +26,15 @@ class MainActivityTest {
     fun appLaunchesSuccessfully() {
         ActivityScenario.launch(MainActivity::class.java)
     }
+
+    //Tests display of nav_view
+    @Test
+    fun onLaunchNavViewDisplayed(){
+        ActivityScenario.launch(MainActivity::class.java)
+
+        onView(withId(R.id.nav_view))
+            .check(matches(isDisplayed()))
+    }
+
+    //Test view change on click of notifications
 }
