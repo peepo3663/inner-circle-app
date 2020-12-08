cd4ecd3acd4c3b558b89755cb17c3467588e6668
Michael Ranasinghe <michael.ranasinghe1@gmail.com>
2020-12-07 23:10:48 -0500

    Handing Off Creating New Chat Room to Po to Integrate with Backend

--- Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/chat/QueriedUserEmailChatFragment.kt
+++ Code/Android/app/src/main/java/edu/bu/metcs673/project/ui/chat/QueriedUserEmailChatFragment.kt
@@ -15,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.ListenerRegistration
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -24,6 +25,7 @@ import edu.bu.metcs673.project.adapter.chat.IndividualChat
 import edu.bu.metcs673.project.adapter.chat.SearchAdapter
 import edu.bu.metcs673.project.model.chat.MessageModel
 import edu.bu.metcs673.project.model.chat.UserEmailModel
+import edu.bu.metcs673.project.model.message.ChatRoomModel
 import kotlinx.android.synthetic.main.fragment_chat.*
 import kotlinx.android.synthetic.main.fragment_chat.user_text_chatbox
 import kotlinx.android.synthetic.main.fragment_chat_query.*
@@ -89,7 +91,7 @@ class QueriedUserEmailChatFragment : AppCompatActivity(){
     private fun retrieveMessages(senderId: String?, receiverId: String)
     {
         mChatList=ArrayList()
-        val reference= chatsRef.document("12MCY06Cac6jg5dkOkgJ").collection("messages")
+        val reference= chatsRef.document("4112T5vcMEnnu7ltmX96").collection("messages")
 
         reference.addSnapshotListener{ messages, _ ->
             (mChatList as ArrayList<IndividualChat>).clear()
@@ -119,9 +121,68 @@ class QueriedUserEmailChatFragment : AppCompatActivity(){
         MessageHashMap["receiver"]=receiverId
         MessageHashMap["message"]=message
 
-        chatsRef.document("12MCY06Cac6jg5dkOkgJ").collection("messages").add(MessageHashMap)
+
+            //val length = 10
+            //val uniquechatid = getRandomString(length)
+
+            var chatroomUser1: String=""
+            var chatroomUser2: String=""
+            val chatroomid=""
+
+/*
+
+            //To do: December 7, 2020
+            //Check if the chat exists
+            chatsRef.addSnapshotListener{ chatrooms, _ ->
+
+                val documents=chatrooms!!.documents
+                documents.forEach{
+                    val singlechatroomObject=it.toObject(ChatRoomModel::class.java)
+                    chatroomUser1 = singlechatroomObject!!.userIds!![0]
+                    chatroomUser2 = singlechatroomObject!!.userIds!![1]
+
+                    // Check whether chatroom exists for the current sender and receiver
+                    if ((chatroomUser1.equals(senderId) || chatroomUser1.equals(receiverId)) &&
+                            (chatroomUser2.equals(senderId) || chatroomUser2.equals(receiverId)))
+                    {
+                        //chatsRef.document(chatroomid).collection("messages").add(MessageHashMap)
+                        val chatroomid=it.id
+                    }
+                }
+
+                // If chatroom id not found this user and reciever, create one now
+                if (chatroomid=="")
+                {
+                    // Create a new chat room for these users
+                    val newchatroom =  HashMap<String, Any?>()
+                    var userIDarray= listOf(senderId,receiverId)
+
+                    newchatroom["userIds"]=userIDarray
+                    newchatroom["createdAt"]= FieldValue.serverTimestamp()
+
+                    //chatsRef.document("4112T5vcMEnnu7ltmX96").collection("messages").add(newchatroom)
+                    val chatroomid=chatsRef.document()
+                    chatroomid.collection("messages").add(newchatroom)
+                    chatsRef.add(newchatroom)
+
+                }
+                else
+                {
+                    //Chatroom ID exist for this sender and receiver
+                    chatsRef.document(chatroomid).collection("messages").add(MessageHashMap)
+                }
+
+
+            }
+
+
+        chatsRef.document("4112T5vcMEnnu7ltmX96").collection("messages").add(MessageHashMap)
+
+ */
 
     }
+
+
 
 
 }
